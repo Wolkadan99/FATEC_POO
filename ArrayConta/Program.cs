778b47b4285a00eb76a5e2679a745347a667f009
Wolkadan99 <marcosgjdj@gmail.com>
2021-08-21 08:56:28 -0300

    Exemplo de Vetor

--- ArrayConta/Program.cs
+++ ArrayConta/Program.cs
@@ -11,10 +11,27 @@ namespace ArrayConta
         static void Main(string[] args)
         {
             Conta[] vetorConta = new Conta[3];//declaração do vetor
-            for(int i=0, i < vetorConta.Length; i++)
+            for (int i = 0; i < vetorConta.Length; i++)
             {
                 vetorConta[i] = new Conta();
+                Console.Write("Informe o número da conta: ");
+                vetorConta[i].numero = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Informe o titular da conta: ");
+                vetorConta[i].titular = Console.ReadLine();
+                Console.Write("Informe o saldo em conta: ");
+                vetorConta[i].saldo = Convert.ToDouble(Console.ReadLine());
             }
+            Console.WriteLine("Exemplo com for .\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t");
+            for (int i = 0; i < vetorConta.Length; i++)
+            {
+                vetorConta[i].Mostrar();
+            }
+            Console.WriteLine("Exemplo com foreach .\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t.\t");
+            foreach (Conta conta in vetorConta)
+            {
+                conta.Mostrar();
+            }
+            Console.ReadKey();
         }
     }
 }
