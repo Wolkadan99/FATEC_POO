f8b2fd5084d44be246dd43a5c9cad57619368326
Wolkadan99 <marcosgjdj@gmail.com>
2021-08-23 20:52:06 -0300

    Aula de encapsulamento.

---
+++ EncapsulamentoFuncionario/Funcionario.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulamentoFuncionario
+{
+    class Funcionario
+    {
+        // declaração de atributos
+        private int codigo;
+        private string nome;
+        private double salario;
+
+        // declaração do encapsulamento
+        public int Codigo
+        {
+            set
+            {
+                this.codigo = value;
+            }
+            get
+            {
+                return this.codigo;
+            }
+        }
+        public string Nome
+        {
+            set
+            {
+                this.nome = value;
+            }
+            get
+            {
+                return this.nome;
+            }
+        }
+        public double Salario
+        {
+            set
+            {
+                this.salario = value;
+            }
+            get
+            {
+                return this.salario;
+            }
+        }
+        // declaração de métodos-funções
+        public void MostrarAtributos()
+        {
+            Console.WriteLine($"Código: {codigo}  Nome: {nome}  Salario: {salario}");
+        }
+    }
+}
---
+++ EncapsulamentoFuncionario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulamentoFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario1 = new Funcionario();
+            funcionario1.Codigo = 1; //Quando atribui valor, o método set esta sendo acionado
+            funcionario1.Nome = "Marco";
+            funcionario1.Salario = 50;
+            Console.WriteLine($"Funcionario {funcionario1.Nome}");
+
+            Funcionario funcionario2 = new Funcionario();
+
+            Console.ReadKey();
+        }
+    }
+}
