b9d00d5aba1f6b6c12ae046aff6587a353a8e2ba
Wolkadan99 <marcosgjdj@gmail.com>
2021-09-04 09:14:29 -0300

    Exercicio em aula.

---
+++ ConstrutorFuncionario/Funcionario.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstrutorFuncionario
+{
+    class Funcionario
+    {
+        //declaração dos atributos
+        private int codigo;
+        private string nome;
+        private double salario;
+        private static int contador;
+
+        public static int Contador
+        {
+            get
+            {
+                return contador;
+            }
+        }
+
+        //declaração dos métodos construtores
+        public Funcionario()
+        {
+            contador++;
+        }
+        public Funcionario(int codigo)
+        {
+            this.codigo = codigo;
+            contador++;
+        }
+        public Funcionario(int codigo, string nome)
+        {
+            this.codigo = codigo;
+            this.nome = nome;
+            contador++;
+
+        }
+        public Funcionario(int codigo, string titular, double salario)
+        {
+            this.codigo = codigo;
+            this.nome = titular;
+            this.salario = salario;
+            contador++;
+        }
+        public void MostrarAtributos()
+        {
+            Console.WriteLine($"\nNúmero de matricula: {codigo}\tFuncionário:{nome}\tSalario:{salario:C}");
+        }
+    }
+
+}
+
---
+++ ConstrutorFuncionario/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConstrutorFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario1 = new Funcionario();
+            funcionario1.MostrarAtributos();
+
+            Funcionario funcionario2 = new Funcionario(50);
+            funcionario2.MostrarAtributos();
+
+            Funcionario funcionario3 = new Funcionario(51, "Alberto");
+            funcionario3.MostrarAtributos();
+
+            Funcionario funcionario4 = new Funcionario(52, "Junberto", 1500);
+            funcionario4.MostrarAtributos();
+            
+            Console.WriteLine($"                                                                    Registros[{Funcionario.Contador}]");            
+            Console.ReadKey();
+        }
+    }
+}
